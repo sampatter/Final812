e233ca7bfed4876c9b050a70cb9130e09a78c3b6
sampatter <36014344+sampatter@users.noreply.github.com>
2018-04-18 11:27:04 -0400

    Update Polynomial_Regressions.R

--- Polynomial_Regressions.R
+++ Polynomial_Regressions.R
@@ -1,6 +1,9 @@
+#This R file was used as model calbration and selction for an overall growth curve
+#Polynomial regressions where fit to all six sample dates - while growth rates are calulated on only 3 dates (25,42,55)
 library(ggplot2)
 #pd<-position_dodge(width=10)
 theme_set(theme_bw())
+#inital Plot 
 pl <- ggplot(Near,aes(x=BDDAY1, y=avg)) +
                geom_point(aes(x=BDDAY1, y=avg), size=2, colour="#993399") +
   xlab("Experimental Day") + ylab("Body Length (cm)") +
@@ -8,24 +11,24 @@ pl <- ggplot(Near,aes(x=BDDAY1, y=avg)) +
 print(pl)
 
 
-#Polynomial 0 Model
+#Polynomial 0 Model - test model 
 attach(BB)
 lm0 <- lm(avg ~ 1)
 lm0
 mean(avg)
 pl + geom_hline(yintercept=coef(lm0)[1],size=1, colour="#339900")
 
-#Polynomial of a 1st degree
+#Polynomial of a 1st degree + test model 
 lm1 <- lm(avg ~ BDDAY1, data=BB)
 coef(lm1)
 summary(lm1)
 confint(lm1)
-#Diagnostic Plots
+#Diagnostic Plots - test model 
 pl  + geom_abline(intercept=coef(lm1)[1],slope=coef(lm1)[2],size=1, colour="#339900")
 par(mfrow = c(1, 2))
 plot(lm1, which=c(1,2))
 
-#predictive power of model 1
+#predictive power of model 1 - Model diagnostic plots and testing with extreme values 
 set.seed(100)
 n <- nrow(A)
 i.training <- sort(sample(n,round(n*0.8)))
@@ -41,11 +44,11 @@ plot(pred1b.test, Length.test)
 abline(a=0, b=1, lty=2)
 plot(pred1b.test, pred1a.test)
 abline(a=0, b=1, lty=2)
-
 cor.test <- cor(pred1a.test, Length.test)
 R2.test <- cor.test^2
 R2.test
-#exterme values
+
+#exterme values test
 i.training <- c(6:200)
 A.training <- c[i.training,]
 A.test  <- A[-i.training,]
@@ -158,7 +161,7 @@ summary.lm(lm3.noint)
 anova(lm3.noint,lm3)
 
 #Data Transformation
-
+#try ploting with a log transformation just for fun - could be linear
 library(gridExtra)
 pl1 <- pl + scale_x_log10()
 pl2 <- pl + scale_y_log10()
@@ -177,7 +180,8 @@ alpha <- 0.05
 df.new <- data.frame(Day=(14:82))
 conf.Length <- predict(lm1.log, newdata = df.new, interval="confidence", level=1-alpha)
 pred.Length <- predict(lm1.log, newdata = df.new, interval="prediction", level=1-alpha)
-#Graphs for log trasnformed
+
+#Graphs for log trasnformed 
 df.new[c("fit","lwr.conf", "upr.conf")] <- conf.Length
 df.new[c("lwr.pred", "upr.pred")] <- pred.Length[,2:3]
 ggplot(A) + geom_point(aes(x=log(Day), y=(Length)), size=2, colour="#993399") +
@@ -188,7 +192,7 @@ ggplot(A) + geom_point(aes(x=log(Day), y=(Length)), size=2, colour="#993399") +
 
 
 
-
+#Exponetial values plot 
 ggplot(A) + geom_point(aes(x=Day, y=exp(Length), size=2, colour="#993399")) +
   geom_ribbon(data=df.new, aes(x=Day, ymin=exp(lwr.pred), ymax=exp(upr.pred)), alpha=0.1, inherit.aes=F, fill="blue") +
   geom_ribbon(data=df.new, aes(x=Day, ymin=exp(lwr.conf), ymax=exp(upr.conf)), alpha=0.2, inherit.aes=F, fill="#339900") +
