bb65b1c471f37b068d0fc917cf3f89a2266be594
owlexmc <36014337+owlexmc@users.noreply.github.com>
2018-04-19 09:30:24 -0400

    Update ClimSum.R

--- ClimSum.R
+++ ClimSum.R
@@ -8,26 +8,20 @@ clim <- read.csv("2016-2017.csv")
 
 
 library(dplyr)
-clim <- clim %>% rename(Date = ï..Date.Time) #renaming date column
+clim <- clim %>% rename(Date = Ã¯..Date.Time) #renaming date column
 #only selecting data from Nov 2016 - March 2017, from first snow fall to when data was collected
 
 
 clim16 <- subset(clim, Year == "2016" & Month == "11" |Month == "12")
 clim17 <- subset(clim, Year == "2017" & Month == "1" | Month == "2" | Month == "3")
-clim2 <- rbind(clim16, clim17) #i think this worked?
+clim2 <- rbind(clim16, clim17) 
 
 
 clim3 <- clim2 %>% select(Date, Snow.on.Grnd..cm., Dir.of.Max.Gust..10s.deg., Spd.of.Max.Gust..km.h.) #creating a dataset for just the variables of interest
-
-#now what do I do with this?
 
 #mean wind direction to add to map
 winddir <- mean(clim3$Dir.of.Max.Gust..10s.deg., na.rm = TRUE)*10
 median(clim3$Dir.of.Max.Gust..10s.deg., na.rm = TRUE)*10
 #Mean direction = 241.9 degrees
 #Median direction = 270
-
-#selecting data when snow is present (when snow on ground =/= NA) - not sure if i should actually do this
-
-
 
